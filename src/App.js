5b8ba2e2e88aa52b7cf1d66a183fd6bdac64a9cc
codewithshamim <shamimislamonline@gmail.com>
2022-04-16 22:43:25 +0600

    added header & footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Shared/Header";
+import Footer from "./components/Shared/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Shared/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: "0",
+        width: "100%",
+        height: "60px",
+        background: "green",
+      }}
+    >
+      <p>Copyright &copy; 2022</p>
+    </div>
+  );
+};
+
+export default Footer;
